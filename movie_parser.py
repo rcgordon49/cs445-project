97e983f6271d45cc74944c43225a5f58d10e9fe7
rgordon <rcgordon@umass.edu>
2015-03-06 12:41:48 -0500

    parser edits

--- movie_parser.py
+++ movie_parser.py
@@ -1,4 +1,5 @@
 import re
+import collections
 
 SOURCE_DIR = 'data/orig_data'
 OUTPUT_DIR = 'data/rel_data'
@@ -6,14 +7,14 @@ OUTPUT_DIR = 'data/rel_data'
 
 #regular expressions for each line of a movie entry
 regex = {'title' : r"\t(.+)",
-         'year' : r"\t(\d{4})", #year has 4 digits
+         'year' : r"\t(\d+)", #year has 4 digits
          'runtime' : r"\t(\d+)",
          'mpaa' : r"\t(\w{1,4})", #longest letter combo is pg13
          'keywords' : r"\t([^\t]+)", #each entry is preceded by a tab
          'producers' : r"\t([^\t]+)",
          'directors' : r"\t([^\t+])",
          'editors' : r"\t([^\t]+)",
-         'actor' : r"(?P<type>Actor|Actress)\t(?P<name>[^\t]+)\t(?P<role>[^\t]*)"
+         'actor' : r"(?P<type>Actor|Actress)\t(?P<name>[^\t]+)\t(?P<role>[^\t]+)"
         }
              
 def findCase(line):
@@ -37,6 +38,22 @@ def findCase(line):
     return 'actor'
   
 
+def createActObj():
+  #actObj = {'pro_name' : '', 'title' : '', 'year' : '', 'role' : ''}
+  actObj = collections.OrderedDict()
+  actObj['pro_name'] = ''
+  actObj['title'] = ''
+  actObj['year'] = ''
+  actObj['role'] = ''
+  return actObj
+  
+def createProObj():
+  #proObj = {'name' : '', 'gender' : ''}
+  proObj = collections.OrderedDict()
+  proObj['name'] = ''
+  proObj['gender'] = ''
+  return proObj
+
 #example order w/ everything --> $windle
 #has to have: title, year lines
 #may or may not have: runtime, key words, mpaa rating
@@ -45,8 +62,9 @@ def findCase(line):
 #x.groupdict()
 log = True
 
-with open('%s/movies.txt' % SOURCE_DIR, 'r') as inFile, open('%s/movies_formatted.txt' % OUTPUT_DIR, 'w+') as movFile, open('%s/professional_formatted.txt' % OUTPUT_DIR, 'w+') as profFile, open('%s/keywords_formatted.txt' % OUTPUT_DIR, 'w+') as keyFile,open('%s/directs_formatted.txt' % OUTPUT_DIR, 'w+') as dirFile,open('%s/produces_formatted.txt' % OUTPUT_DIR, 'w+') as prodFile,open('%s/edits_formatted.txt' % OUTPUT_DIR, 'w+') as edFile,open('%s/actsIn_formatted.txt' % OUTPUT_DIR, 'w+') as actRelFile:
-  #movFile, profFile, keyFile, dirFile, prodFile, edFile, actRelFile
+with open('%s/movies.txt' % SOURCE_DIR, 'r', encoding='latin-1') as inFile, open('%s/movies_formatted.txt' % OUTPUT_DIR, 'w+') as movFile, open('%s/professional_formatted.txt' % OUTPUT_DIR, 'w+') as profFile, open('%s/keywords_formatted.txt' % OUTPUT_DIR, 'w+') as keyFile,open('%s/directs_formatted.txt' % OUTPUT_DIR, 'w+') as dirFile,open('%s/produces_formatted.txt' % OUTPUT_DIR, 'w+') as prodFile,open('%s/edits_formatted.txt' % OUTPUT_DIR, 'w+') as edFile,open('%s/actsIn_formatted.txt' % OUTPUT_DIR, 'w+') as actRelFile:
+  #movFile, profFile, keyFile, 
+  #dirFile, prodFile, edFile, actRelFile
   for line in inFile:
     case = findCase(line)
     
@@ -68,26 +86,36 @@ with open('%s/movies.txt' % SOURCE_DIR, 'r') as inFile, open('%s/movies_formatte
       if log: print('mpaa: %s' % movieObj['mpaa'])
    
     elif case == 'keywords':
-      if log: print('keyword')
+      keywords = re.findall(regex['keywords'], line)
+      keyFile.write('\n'.join(keywords))
+      if log: print(','.join(keywords))
     
     elif case == 'producers':
-      if log: print('producers')
+      producers = re.findall(regex['producers'], line)
+      keyFile.write('\n'.join(producers))
+      if log: print(','.join(producers))
       
     elif case == 'directors':
-      if log: print('directors')
+      directors = re.findall(regex['directors'], line)
+      keyFile.write('\n'.join(directors))
+      if log: print(','.join(directors))
       
     elif case == 'editors':
-      if log: print('editors')
+      editors = re.findall(regex['editors'], line)
+      keyFile.write('\n'.join(editors))
+      if log: print(','.join(editors))
       
     elif case == 'actor':
-      proObj = {'name' : '', 'gender' : ''}
-      actObj = {'pro_name' : '', 'title' : '', 'year' : '', 'role' : ''}
+      proObj = createProObj()
+      actObj = createActObj()
       
       m = re.match(regex['actor'], line).groupdict()
       proObj['name'] = m['name']
       proObj['gender'] = 'M' if m['type'] == 'Actor' else 'F'
       actObj['pro_name'] = m['name']
       actObj['role'] = m['role']
+      actObj['title'] = movieObj['title']
+      actObj['year'] = movieObj['year']
       
       profFile.write('\t'.join(proObj.values()) + '\n')
       profFile.flush()
@@ -97,6 +125,9 @@ with open('%s/movies.txt' % SOURCE_DIR, 'r') as inFile, open('%s/movies_formatte
       if log:
         print('actor obj: %s' % str(actObj))
         print('pro obj: %s' % str(proObj))
+        
+    else:
+      print('invalid case')
       
       
       
