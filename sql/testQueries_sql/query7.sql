3726b95d8cd3f30475c21832c75d0406e3ca8a60
Ryan Stanley <stanleyrya@gmail.com>
2015-03-30 11:28:17 -0400

    query 7

---
+++ sql/testQueries_sql/query7.sql
@@ -0,0 +1,4 @@
+SELECT U.email_address, U.name, U.age, U.location
+FROM Users U, Rates R, Has_Mpaa_Rating H
+WHERE U.age <= 17 AND U.email_address = R.email_address
+	AND R.mid = H.mid AND H.mpaa_rating = "NC-17";
