ac0b20481903185df431e64f89b603ff3477fd53
rgordon <rcgordon@umass.edu>
2015-03-30 12:19:37 -0400

    fixed view for query 8, fixed query 9

---
+++ sql/testQueries_sql/query8_view.sql
@@ -0,0 +1,9 @@
+CREATE VIEW Best_Movies
+AS
+SELECT DISTINCT M.mid, M.title, M.year, avg(R.user_rating)
+FROM Movies M, Rates R
+WHERE R.mid = M.mid
+GROUP BY M.mid
+HAVING count(*) > 1000
+ORDER BY avg(R.user_rating) DESC
+LIMIT 5;
