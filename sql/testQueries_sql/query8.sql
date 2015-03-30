83aa72011ed369e295b238b61054c4594122d651
rgordon <rcgordon@umass.edu>
2015-03-30 13:04:59 -0400

    renamed avg column in query8

--- sql/testQueries_sql/query8.sql
+++ sql/testQueries_sql/query8.sql
@@ -1,7 +1,7 @@
-SELECT DISTINCT M.title, M.year, avg(R.user_rating)
+SELECT DISTINCT M.title, M.year, avg(R.user_rating) AS avg_rating
 FROM Movies M, Rates R
 WHERE R.mid = M.mid
-GROUP BY M.mid
+GROUP BY R.mid
 HAVING count(*) > 1000
-ORDER BY avg(R.user_rating) DESC
+ORDER BY avg_rating DESC
 LIMIT 5;
