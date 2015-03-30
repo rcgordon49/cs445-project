37291c25e5cb2d126e9ab37ae931b4ea505a3c8c
rgordon <rcgordon@umass.edu>
2015-03-30 12:27:36 -0400

    altered query 9 to work with a best_movies view that has top 10 movies instead of top 5

--- sql/testQueries_sql/query9.sql
+++ sql/testQueries_sql/query9.sql
@@ -1,5 +1,7 @@
 SELECT DISTINCT R.email_address
-FROM Rates R, Best_Movies B
+FROM Rates R, (SELECT * 
+               FROM Best_Movies B2 
+               ORDER BY B2.avg_rating DESC LIMIT 5) AS B
 WHERE R.mid = B.mid
 GROUP BY R.email_address
 HAVING COUNT(*) >= 2 AND MIN(R.user_rating) >= 9;
