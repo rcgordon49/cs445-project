a502712588d32dca234658d8cd4c84ea09c04fd1
rgordon <rcgordon@umass.edu>
2015-04-08 14:41:09 -0400

    this is actually where the table is altered to include an avg_age col for Avg_Ratings

--- sql/testQueries_sql/query8_helper_table.sql
+++ sql/testQueries_sql/query8_helper_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE Avg_Ratings
  year        INT(4) NOT NULL,
  num_rates   INT,
  avg_rating  DOUBLE,
+ avg_user_age INT,
  FOREIGN KEY (mid) REFERENCES Movies(mid)
  ON DELETE CASCADE
  ON UPDATE CASCADE,
@@ -14,9 +15,10 @@ CREATE TABLE Avg_Ratings
  PRIMARY KEY (mid));
  
 
-INSERT INTO Avg_Ratings(mid, title, year, num_rates, avg_rating)
+INSERT INTO Avg_Ratings(mid, title, year, num_rates, avg_rating, avg_user_age)
 SELECT DISTINCT M.mid, M.title, M.year, 
-    count(R.user_rating) as num_rates, avg(R.user_rating) AS avg_rating
-FROM Movies M, Rates R
-WHERE R.mid = M.mid
+    count(R.user_rating) as num_rates, avg(R.user_rating) AS avg_rating,
+    avg(U.age) AS avg_user_age
+FROM Movies M, Rates R, Users U
+WHERE R.mid = M.mid AND R.email_address = U.email_address
 GROUP BY R.mid;
