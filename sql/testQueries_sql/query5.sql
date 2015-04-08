3e6a72a428c2a3479d2653c991cf88bf785bea93
Ryan Stanley <stanleyrya@gmail.com>
2015-04-08 13:47:41 -0400

    this is the index I used

--- sql/testQueries_sql/query5.sql
+++ sql/testQueries_sql/query5.sql
@@ -3,3 +3,5 @@ FROM Movies M, Directs D, Rates R, Users U
 WHERE R.email_address = U.email_address AND R.mid = M.mid AND D.mid = M.mid
   AND U.name = "DERRICK MYERS" AND U.age = 36
   AND R.user_rating = 10;
+
+-- CREATE INDEX part_of_name ON customer (name(10));
