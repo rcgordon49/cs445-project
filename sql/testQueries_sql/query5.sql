41c29d6b465c6000b3f43b140d98b7a600d6d6d8
Ryan Stanley <stanleyrya@gmail.com>
2015-04-08 14:21:59 -0400

    index commands

--- sql/testQueries_sql/query5.sql
+++ sql/testQueries_sql/query5.sql
@@ -4,4 +4,4 @@ WHERE R.email_address = U.email_address AND R.mid = M.mid AND D.mid = M.mid
   AND U.name = "DERRICK MYERS" AND U.age = 36
   AND R.user_rating = 10;
 
--- CREATE INDEX part_of_name ON customer (name(10));
+-- CREATE INDEX name_index ON Users (name);
