4b36b83075d18d12c96f2782a4aa4ffd5e6e3593
Nick Delfino <delfinonick@gmail.com>
2015-04-17 10:34:50 -0400

    Folder + file
    Added folder and file for php

---
+++ website/test.php
@@ -0,0 +1,23 @@
+<html>
+ <head>
+     <title>My cool website</title>
+ </head>
+ <body>	
+	<div align = "right">
+	<button align="right" type="button">Sign In</button>
+	</div>
+	 <!Main title for website>
+     <div align = "center" id="header">
+         <!img src="templates\logo.png" alt="Cool Website Logo" />
+		 <p style="font-size: 200%;" >MovieNet</p>
+     </div>
+     <div id="content">
+<?php
+	//include("templates/header.htm");
+	//include("templates/index.htm");
+	//include("templates/footer.htm");
+	echo "this is so cool"
+?>
+<div align = "center" >
+	<input onfocus="this.value=''" value="Enter Movie Here" type="text" id="text" name="text_name" style="width: 300px;" />
+	
