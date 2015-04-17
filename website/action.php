208d0e75856e6cdc7d90f37a1181bdf1b16ce341
Ryan Stanley <stanleyrya@gmail.com>
2015-04-17 12:06:52 -0400

    query works with input!

--- website/action.php
+++ website/action.php
@@ -1,36 +1,31 @@
 <html><body>
 <?php
-$name = $_POST['name'];
-$age = $_POST['age'];
+	$title = $_POST['title'];
 
-echo $name;
-echo $age;
+	echo $title;
 
-?>
+	$connection = mysql_connect("cs445sql", "rwstanle", "EL310rws");
+	if (!$connection){
+		die ("Couldn't connect to mysql server!<br>The error was: " . mysql_error());
+	}
+	else{
+		echo "Connection successful!<br>\n";
+	}
+	if (!mysql_select_db("DGS"))
+		die ("Couldn't select a database!<br> Error: " . mysql_error());
+	else
+		echo "Databse selected successfully.<br>\n";
 
-<?php
-  $connection = mysql_connect("cs445sql", "rwstanle", "EL310rws");
-  if (!$connection){
-    die ("Couldn't connect to mysql server!<br>The error was: " . mysql_error());
-  }
-  else{
-    echo "Connection successful!<br>\n";
-  }
-  if (!mysql_select_db("DGS"))
-    die ("Couldn't select a database!<br> Error: " . mysql_error());
-  else
-    echo "Databse selected successfully.<br>\n";
-
-  $query = "SELECT DISTINCT M.mid, M.title, M.year FROM Movies M WHERE M.title LIKE 'Life%'";
-  $result = mysql_query($query);
-  if (!$result)
-    echo "Query failed!";
-  else{
-    echo "Query successful.<br>\n";
-    echo "Songs:<br>";
-    while ($row = mysql_fetch_array($result)){
-      echo "mid: $row[0], title: $row[1], year: $row[2]<br>\n";
-    }
-  }
+	$query = "SELECT DISTINCT M.mid, M.title, M.year FROM Movies M WHERE M.title LIKE '" . $title . "%'";
+	$result = mysql_query($query);
+	if (!$result)
+		echo "Query failed!";
+	else{
+		echo "Query successful.<br>\n";
+		echo "Songs:<br>";
+		while ($row = mysql_fetch_array($result)){
+			echo "mid: $row[0], title: $row[1], year: $row[2]<br>\n";
+		}
+	}
 ?>
 </body></html>
--- website/test.php
+++ website/test.php
@@ -4,8 +4,7 @@
  </head>
  <body>	
 	<form action="action.php" method="post">
-	 <p>Your name: <input type="text" name="name" /></p>
-	 <p>Your age: <input type="text" name="age" /></p>
+	 <p>Movie title: <input type="text" name="title" /></p>
 	 <p><input type="submit" /></p>
 	</form>
 </body>
