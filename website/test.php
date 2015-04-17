df2fcbe37f28aaa00143155b834dccc40fbfceb9
Ryan Stanley <stanleyrya@gmail.com>
2015-04-17 11:58:55 -0400

    calls database!!

--- website/test.php
+++ website/test.php
@@ -3,21 +3,10 @@
      <title>My cool website</title>
  </head>
  <body>	
-	<div align = "right">
-	<button align="right" type="button">Sign In</button>
-	</div>
-	 <!Main title for website>
-     <div align = "center" id="header">
-         <!img src="templates\logo.png" alt="Cool Website Logo" />
-		 <p style="font-size: 200%;" >MovieNet</p>
-     </div>
-     <div id="content">
-<?php
-	//include("templates/header.htm");
-	//include("templates/index.htm");
-	//include("templates/footer.htm");
-	echo "this is so cool"
-?>
-<div align = "center" >
-	<input onfocus="this.value=''" value="Enter Movie Here" type="text" id="text" name="text_name" style="width: 300px;" />
-	
+	<form action="action.php" method="post">
+	 <p>Your name: <input type="text" name="name" /></p>
+	 <p>Your age: <input type="text" name="age" /></p>
+	 <p><input type="submit" /></p>
+	</form>
+</body>
+</html>
