49514c39f70cc4cf4a96ee08078dfa0c364ef80c
Ryan Stanley <stanleyrya@gmail.com>
2015-04-24 11:40:59 -0400

    extracted bar.php

--- website/index.php
+++ website/index.php
@@ -12,22 +12,10 @@
 		<p style="font-size: 200%;" >MovieNet</p>
 	</div>
 
-
-
 	<div align = "center" >
-		<form method="POST" action="action.php">
-			<input type="text" size="48" name="search"></label>
-
-			<select name = "query_type" id="query_type">
-				<option value="title">Title</option>
-				<option value="professional">Professional</option>
-				<option value="genre">Genre</option>
-				<option value="year">Year</option>
-				<option value = "user">User</option>
-			</select>
-
-			<input type="submit" value="Search" align="right">
-		</form>
+  		<?php
+  			include("bar.php");
+  		?>
 	<div/>
 </body>
 </html>
