b7933998c6743cdeef5c6e07a6333e8246ae56b1
Nick Delfino <delfinonick@gmail.com>
2015-04-27 16:49:17 -0400

    Added button in index to redirect

--- website/index.php
+++ website/index.php
@@ -2,10 +2,15 @@
 <head>
 	<title>My cool website</title>
 </head>
+
 <body>	
+	<?php include("import.php"); ?>
 	<div align = "right">
-		<button align="right" type="button">Sign In</button>
+		<form action="http://cs445.cs.umass.edu/php-wrapper/cs445_DGS_s15/login.html" align="right">
+    		<input type="submit" value="Sign In">
+		</form>
 	</div>
+
 	<!Main title for website>
 	<div align = "center" id="header">
 		<!img src="templates\logo.png" alt="Cool Website Logo" />
