6e0431df9463f54eff9a0ecda384c3195e059256
Nick Delfino <delfinonick@gmail.com>
2015-04-27 20:38:58 -0400

    Adjustments to logging in and signing up

--- website/signupAction.php
+++ website/signupAction.php
@@ -1,7 +1,8 @@
 <?php
 session_start(); // Starting Session
 $error=''; // Variable To Store Error Message
-if (isset($_POST['submit'])) {
+
+include("dbConnect.php");
 
 // Define $username and $password
 		$email=$_POST['email'];
@@ -12,33 +13,72 @@ if (isset($_POST['submit'])) {
 		$age=$_POST['age'];
 		$gender=$_POST['gender'];
 
-		if(strcmp($password, $confirmpassword) == 0){
+		console.log('$email');
 
-			// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-					$connection = mysql_connect("localhost", "root", "");
-			// To protect MySQL injection for Security purpose
-					$username = stripslashes($username);
-					$password = stripslashes($password);
-					$username = mysql_real_escape_string($username);
-					$password = mysql_real_escape_string($password);
-			// Selecting Database
-					$db = mysql_select_db("company", $connection);
-			// SQL query to fetch information of registerd users and finds user match.
-					$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
-					$rows = mysql_num_rows($query);
-					if ($rows == 1) {
-					$_SESSION['login_user']=$username; // Initializing Session
-					header("location: profile.php"); // Redirecting To Other Page
+		$checkEmailQuery = "SELECT * FROM Users U WHERE U.email_address = '$email'";
+
+		$checkEmailQueryResults = mysql_query($watchQuery);
+		$checkEmailQueryCount = mysql_num_rows($checkEmailQueryResults);
+
+		if($checkEmailQueryCount == 0){
+
+			if(strcmp($password, $confirm) == 0){
+
+						
+						// To protect MySQL injection for Security purpose
+						$email = stripslashes($email);
+						$password = stripslashes($password);
+
+						if(isset($_POST['name'])){
+							$name=stripslashes($_POST['name']);
+						}else{
+							$name= "No Name";
+						}
+						
+						if(isset($_POST['location'])){
+							$location = stripslashes($_POST['location']);
+						}else{
+							$location = "No Location";
+						}
+						
+						if(isset($_POST['age'])){
+							$age = stripslashes($_POST['age']);
+						}else{
+							$age = "No Age";
+						}
+
+						if(isset($_POST['gender'])){
+							$gender = stripslashes($_POST['gender']);
+						}else{
+							$gender = "No Gender";
+						}
+						
+						
+
+						$username = mysql_real_escape_string($username);
+						$password = mysql_real_escape_string($password);
+						$name = mysql_real_escape_string($name);
+						$location = mysql_real_escape_string($location);
+						$age = mysql_real_escape_string($age);
+						$gender = mysql_real_escape_string($gender);
+
+						
+						// SQL query to fetch information of registerd users and finds user match.
+						$insertNewUserQuery = "INSERT INTO Users(email,name,password,age,gender,location) 
+													VALUES ('$email','$name','$password','$age','$gender','$location')";
+
+						$query = mysql_query($insertNewUserQuery);
+
+						header("location: user.php?email=$email"); // Redirecting To Other Page
+					
+
+			}else{
+				$error = "Passwords don't match";
+				header("Location: signup.php?error=$error");
+			}
 		}else{
-			$error = "Passwords don't match";
-			header("Location: signup.html?error=$error");
+			$error = "Email already in use";
+			header("Location: signup.php?error=$error");
 		}
-
-
-		} else {
-			$error = "Username or Password is invalid";
-		}
-			mysql_close($connection); // Closing Connection
-}
 
 ?>
